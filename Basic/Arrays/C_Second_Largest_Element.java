a4e707f211753c83deaa6990ea37e096b3786a9d
VinayakKittad <vinay.reactdeveloper@gmail.com>
2024-01-27 14:32:07 +0530

    Added C excerise

--- Basic/Arrays/C_Second_Largest_Element.java
+++ Basic/Arrays/C_Second_Largest_Element.java
@@ -1,3 +1,32 @@
 public class C_Second_Largest_Element {
    
+      public static int secMaxNum(int arr[], int size){
+
+            int maxNum= Integer.MIN_VALUE;
+            int secMaxNum = Integer.MIN_VALUE;
+
+            for(int i=0; i<size; i++){
+                if(maxNum < arr[i]){
+                    secMaxNum =maxNum;
+                    maxNum= arr[i];
+                }
+                else if(arr[i] > secMaxNum && arr[i] < maxNum){
+                     secMaxNum= arr[i];
+                }
+            }
+            if(secMaxNum == Integer.MIN_VALUE) return -1;
+            return secMaxNum;
+
+
+        }
+    public static void main(String args[]){
+
+        int arr[] = {455, 455, 455 ,0,455};
+        int size= arr.length;
+
+        int result = secMaxNum(arr, size);
+        System.out.println("The second Largest number in the array is = "+ result);
+
+
+    }
 }
